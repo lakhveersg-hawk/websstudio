19b1caeede82bfe58a0117a108f02cdc9e1f36c8
unknown <lakhveersg1999@gmail.com>
2024-10-01 13:22:23 +0530

    Your commit message

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,5 +1,5 @@
-import Header from '../../components/Header/header';
-import Footer from '../../components/Footer/footer';
+import Header from '../../components/header/header';
+import Footer from '../../components/footer/footer';
 export default function Contact() {
     return (
         <>
